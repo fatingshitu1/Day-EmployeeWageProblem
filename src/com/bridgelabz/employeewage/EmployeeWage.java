17b3affba5d2227dea4508aba2d313a64105da12
shitalfating <shitalfating84@gmail.com>
2023-01-18 23:35:21 +0530

    Refactored: Calculation of Part Time Wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,26 +1,30 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
-    static final int WAGE_PER_HOUR =20;
-    static final int HOURS_PER_DAY =8;
+    static final int WAGE_PER_HOUR = 20;
+    static final int HOURS_PER_DAY = 8;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Problem");
         //UC1 - Attendance Check
-        int x = (int) (Math.random() * 10 )% 2;
+        int x = (int) (Math.random() * 10) % 2;
         if (x == 0) {
             System.out.println("Employee is Absent");
-        }else{
+        } else {
             System.out.println("Employee is Present");
-        }int employeecheck = (int) (Math.random() * 10 )% 2;
+        }
+        int employeecheck = (int) (Math.random() * 10) % 3;
         System.out.println("employee check");
         int dailyWage = 0;
-        if (employeecheck ==1) {
+        if (employeecheck == 1) {
             System.out.println("Employee is Present");
             dailyWage = WAGE_PER_HOUR * HOURS_PER_DAY;
-        }else{
-            System.out.println("Employee is Absent");
+        } else if (employeecheck == 2) {
+            System.out.println("Employee is working half day");
+            dailyWage = WAGE_PER_HOUR * (HOURS_PER_DAY / 2);
+        } else {
+            System.out.println("Employee is absent");
         }
-        System.out.println("employee get the daily wage is "+ dailyWage);
+        System.out.println("employee get the daily wage is " + dailyWage);
     }
 }
-
