f10cf063895a4119b79b58c874644a5c3a483b43
shitalfating <shitalfating84@gmail.com>
2023-01-18 23:17:33 +0530

    Refactored: Calculate the Daily Wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
+    static final int WAGE_PER_HOUR =20;
+    static final int HOURS_PER_DAY =8;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Problem");
         //UC1 - Attendance Check
@@ -9,6 +11,16 @@ public class EmployeeWage {
             System.out.println("Employee is Absent");
         }else{
             System.out.println("Employee is Present");
+        }int employeecheck = (int) (Math.random() * 10 )% 2;
+        System.out.println("employee check");
+        int dailyWage = 0;
+        if (employeecheck ==1) {
+            System.out.println("Employee is Present");
+            dailyWage = WAGE_PER_HOUR * HOURS_PER_DAY;
+        }else{
+            System.out.println("Employee is Absent");
         }
+        System.out.println("employee get the daily wage is "+ dailyWage);
     }
 }
+
