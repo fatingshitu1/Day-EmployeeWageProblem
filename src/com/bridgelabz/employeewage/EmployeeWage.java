47eb33aa178803e0e956fc41bab202925f0450be
shitalfating <shitalfating84@gmail.com>
2023-01-19 18:06:40 +0530

    Refactored: calculate Hours per month

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,7 +3,9 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
     static final int HOURS_PER_DAY = 8;
-    static final int DAYS_PER_MONTH = 20;
+    static final int PART_TIMES_MONTH= 20;
+    static final int DAYS_PER_MONTH =20;
+    static final int HOURS_PER_MONTH =100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Problem");
@@ -16,10 +18,12 @@ public class EmployeeWage {
             case 1:
             System.out.println("Employee is Present");
             dailyWage = WAGE_PER_HOUR * HOURS_PER_DAY;
+            monthlyWage = dailyWage * DAYS_PER_MONTH;
             break;
             case 2:
             System.out.println("Employee is working half day");
             dailyWage = WAGE_PER_HOUR * (HOURS_PER_DAY / 2);
+            monthlyWage = dailyWage * DAYS_PER_MONTH;
             break;
             default:
             System.out.println("Employee is absent");
