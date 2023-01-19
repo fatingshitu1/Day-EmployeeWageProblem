c53e8fccf5dc3cb61e3f70af6d8210988f25735e
shitalfating <shitalfating84@gmail.com>
2023-01-19 17:47:39 +0530

    Refactored: Employee Wage By Using Switch Case

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -7,22 +7,19 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Problem");
         //UC1 - Attendance Check
-        int x = (int) (Math.random() * 10) % 2;
-        if (x == 0) {
-            System.out.println("Employee is Absent");
-        } else {
-            System.out.println("Employee is Present");
-        }
         int employeecheck = (int) (Math.random() * 10) % 3;
         System.out.println("employee check");
         int dailyWage = 0;
-        if (employeecheck == 1) {
+        switch (employeecheck){
+            case 1:
             System.out.println("Employee is Present");
             dailyWage = WAGE_PER_HOUR * HOURS_PER_DAY;
-        } else if (employeecheck == 2) {
+            break;
+            case 2:
             System.out.println("Employee is working half day");
             dailyWage = WAGE_PER_HOUR * (HOURS_PER_DAY / 2);
-        } else {
+            break;
+            default:
             System.out.println("Employee is absent");
         }
         System.out.println("employee get the daily wage is " + dailyWage);
