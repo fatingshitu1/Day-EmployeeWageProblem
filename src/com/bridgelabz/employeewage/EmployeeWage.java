ae19666873a167202591151096687ccc4a1c2719
shitalfating <shitalfating84@gmail.com>
2023-01-19 21:42:47 +0530

    Refactored:The Code Write With Class Method

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,32 +3,36 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
     static final int HOURS_PER_DAY = 8;
-    static final int PART_TIMES_MONTH= 20;
-    static final int DAYS_PER_MONTH =20;
-    static final int HOURS_PER_MONTH =100;
+    static final int PART_TIMES_MONTH = 20;
+    static final int DAYS_PER_MONTH = 20;
+    static final int HOURS_PER_MONTH = 100;
 
-    public static void main(String[] args) {
+    static void EmployeeWageComputation() {
         System.out.println("Welcome to Employee Wage Problem");
         //UC1 - Attendance Check
         int employeecheck = (int) (Math.random() * 10) % 3;
         System.out.println("employee check");
         int dailyWage = 0;
         int monthlyWage = 0;
-        switch (employeecheck){
+        switch (employeecheck) {
             case 1:
-            System.out.println("Employee is Present");
-            dailyWage = WAGE_PER_HOUR * HOURS_PER_DAY;
-            monthlyWage = dailyWage * DAYS_PER_MONTH;
-            break;
+                System.out.println("Employee is Present");
+                dailyWage = WAGE_PER_HOUR * HOURS_PER_DAY;
+                monthlyWage = dailyWage * DAYS_PER_MONTH;
+                break;
             case 2:
-            System.out.println("Employee is working half day");
-            dailyWage = WAGE_PER_HOUR * (HOURS_PER_DAY / 2);
-            monthlyWage = dailyWage * DAYS_PER_MONTH;
-            break;
+                System.out.println("Employee is working half day");
+                dailyWage = WAGE_PER_HOUR * (HOURS_PER_DAY / 2);
+                monthlyWage = dailyWage * DAYS_PER_MONTH;
+                break;
             default:
-            System.out.println("Employee is absent");
+                System.out.println("Employee is absent");
         }
         System.out.println("employee get the daily wage is " + dailyWage);
         System.out.println("employee get the daily wage is " + monthlyWage);
+    }
+
+    public static void main(String[] args) {
+        EmployeeWageComputation();
     }
 }
